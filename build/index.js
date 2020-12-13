db6cb3c0eb5f732bba975b9693644835690c9a0b
James-Oswald <jamesoswald111@gmail.com>
2020-12-13 02:25:57 -0500

    finished assembler

--- build/index.js
+++ build/index.js
@@ -7,6 +7,7 @@ let errorSize = null;
 
 function start(){
     try{
+        lwtahSetUpdateOnInput(false);
         assembleLine = Module.cwrap("assembleLineW", "null", ["string", "number", "number", "number", "number", "number"]);
         errorSize = Module.cwrap("getMaxErrorSize", "number", []);
         initalized = true;
@@ -23,56 +24,67 @@ function assembleCallback(warning, error){
 function assemble(){
     if(!initalized)
         throw new Error("Can't assemble untill WASM is initilized");
+    let input = document.getElementById("SIAInput");
     let output = document.getElementById("SIAOutput");
     let debug = document.getElementById("SIADebug");
-    output.innerHTML = "";
-    debug.innerHTML = "";
-    let lines = document.getElementById("SIAInput").value.split("\n");
-    let maxErrSize = errorSize();
-    let outputBytesPtr = Module._malloc(4);
-    let numOutputBytesPtr = Module._malloc(4);
-    let errorPtr = Module._malloc(maxErrSize);
-    let warningPtr = Module._malloc(maxErrSize);
-    let inputIndex = 0;
-    let outputIndex = 0;
-    let debugIndex = 0;
+    output.value = "";
+    debug.value = "";
+    let lines = input.value.split("\n");
+
+    //return values
+    let maxErrSize = errorSize();                   //The max length of an error or warning message
+    let outputBytesPtr = Module._malloc(4);         //The machine code of the instruction
+    let numOutputBytesPtr = Module._malloc(4);      //The number length of the machine code
+    let errorPtr = Module._malloc(maxErrSize);      //The pointer to an error message
+    let warningPtr = Module._malloc(maxErrSize);    //The pointer to a warning message
+
+    let tags = [];
+    let processedInput = "";
+    
     for(let i = 0; i < lines.length; i++){
         let line = lines[i];
-        for(let j = 0; j < maxErrSize; j++){
+        for(let j = 0; j < maxErrSize; j++){    //0s out the entire error and warning buffer? 
             Module.HEAPU8.set([0], errorPtr + j)
             Module.HEAPU8.set([0], warningPtr + j);
         }
         assembleLine(line, outputBytesPtr, numOutputBytesPtr, i, errorPtr, warningPtr);
         let numOutputBytes = Module.getValue(numOutputBytesPtr, "i32");
         console.log(numOutputBytes);
-        let outputBytes = new Uint8Array(Module.HEAPU8.buffer, outputBytesPtr, numOutputBytes); //fix this with numOutputBytes after testing
+        //fix this with numOutputBytes after testing <--- OG comment? I think i already did this last year.
+        let outputBytes = new Uint8Array(Module.HEAPU8.buffer, outputBytesPtr, numOutputBytes); 
         let error = String.fromCharCode.apply(null, new Uint8Array(Module.HEAPU8.buffer, errorPtr, maxErrSize));
         let warning = String.fromCharCode.apply(null, new Uint8Array(Module.HEAPU8.buffer, warningPtr, maxErrSize));
         error = error.replace(/\0[\s\S]*$/g,'').trim();
         warning = warning.replace(/\0[\s\S]*$/g,'').trim();
-        if(error != ""){
-            errorStrings[2].push(error);
-            debug.innerHTML += error + "&#13;&#10;"
-            console.warn(error);
-        }
-        if(warning != ""){
-            warningStrings[2].push(warning);
-            debug.innerHTML += warning + "&#13;&#10;"
-            console.warn(warning);
-        }
-        let outputString = "";
+
+        let inputTagStart = processedInput.length;
+        processedInput += line + "\n";
+        let inputTagEnd = processedInput.length;
+
+        let outputTagStart = output.value.length;
         for(let j = 0; j < numOutputBytes; j++)
-            outputString += ("0" + outputBytes[j].toString(16)).slice(-2);
-        output.innerHTML += outputString + "&#13;&#10;";
-        if(error != ""){
-            errorStrings[1].push(outputString);
-            errorStrings[0].push(line);
-        }
-        else if(warning != ""){
-            warningStrings[1].push(outputString);
-            warningStrings[0].push(line);
-        }
-    }    
-    console.log(errorStrings);
-    console.log(warningStrings);
+            output.value += ("0" + outputBytes[j].toString(16)).slice(-2);
+        let outputTagEnd = output.value.length;
+        output.value += "\n";
+
+        let debugTagStart = debug.value.length;
+        if(error != "")
+            debug.value += error + "\n";
+        else if(warning != "")
+            debug.value += warning + "\n";
+        let debugTagEnd = debug.value.length;
+        
+        if(error != "")
+            tags.push([[inputTagStart, inputTagEnd],[outputTagStart, outputTagEnd],[debugTagStart,debugTagEnd], "#f4cccc"]);
+        else if(warning != "")
+            tags.push([[inputTagStart, inputTagEnd],[outputTagStart, outputTagEnd],[debugTagStart,debugTagEnd], "#fff2cc"]);
+    }
+    lwtahClear("SIAInput");
+    lwtahClear("SIAOutput");
+    lwtahClear("SIADebug");
+    for(let i = 0; i < tags.length; i++){
+        lwtahAddRange("SIAInput", tags[i][0], tags[i][3]);
+        lwtahAddRange("SIAOutput", tags[i][1], tags[i][3]);
+        lwtahAddRange("SIADebug", tags[i][2], tags[i][3]);
+    }   
 }
--- src/website/index.js
+++ src/website/index.js
@@ -7,6 +7,7 @@ let errorSize = null;
 
 function start(){
     try{
+        lwtahSetUpdateOnInput(false);
         assembleLine = Module.cwrap("assembleLineW", "null", ["string", "number", "number", "number", "number", "number"]);
         errorSize = Module.cwrap("getMaxErrorSize", "number", []);
         initalized = true;
@@ -23,11 +24,12 @@ function assembleCallback(warning, error){
 function assemble(){
     if(!initalized)
         throw new Error("Can't assemble untill WASM is initilized");
+    let input = document.getElementById("SIAInput");
     let output = document.getElementById("SIAOutput");
     let debug = document.getElementById("SIADebug");
     output.value = "";
     debug.value = "";
-    let lines = document.getElementById("SIAInput").value.split("\n");
+    let lines = input.value.split("\n");
 
     //return values
     let maxErrSize = errorSize();                   //The max length of an error or warning message
@@ -36,12 +38,8 @@ function assemble(){
     let errorPtr = Module._malloc(maxErrSize);      //The pointer to an error message
     let warningPtr = Module._malloc(maxErrSize);    //The pointer to a warning message
 
-    let outputErrorTags = [];
-    let outputWarningTags = [];
-    let debugErrorTags = [];
-    let debugWarningTags = [];
-    let inputErrorTags = [];
-    let inputWarningTags = [];
+    let tags = [];
+    let processedInput = "";
     
     for(let i = 0; i < lines.length; i++){
         let line = lines[i];
@@ -58,20 +56,35 @@ function assemble(){
         let warning = String.fromCharCode.apply(null, new Uint8Array(Module.HEAPU8.buffer, warningPtr, maxErrSize));
         error = error.replace(/\0[\s\S]*$/g,'').trim();
         warning = warning.replace(/\0[\s\S]*$/g,'').trim();
+
+        let inputTagStart = processedInput.length;
+        processedInput += line + "\n";
+        let inputTagEnd = processedInput.length;
+
         let outputTagStart = output.value.length;
         for(let j = 0; j < numOutputBytes; j++)
             output.value += ("0" + outputBytes[j].toString(16)).slice(-2);
         let outputTagEnd = output.value.length;
-        output.value += "/n";
-        
-        let debugTagStart = output.value.length;
-        if(error != "")
-            debug.value += error + "/n"
-        if(warning != "")
-            debug.value += warning + "/n"
-        let debugTagEnd = output.value.length;
+        output.value += "\n";
 
+        let debugTagStart = debug.value.length;
+        if(error != "")
+            debug.value += error + "\n";
+        else if(warning != "")
+            debug.value += warning + "\n";
+        let debugTagEnd = debug.value.length;
+        
+        if(error != "")
+            tags.push([[inputTagStart, inputTagEnd],[outputTagStart, outputTagEnd],[debugTagStart,debugTagEnd], "#f4cccc"]);
+        else if(warning != "")
+            tags.push([[inputTagStart, inputTagEnd],[outputTagStart, outputTagEnd],[debugTagStart,debugTagEnd], "#fff2cc"]);
+    }
+    lwtahClear("SIAInput");
+    lwtahClear("SIAOutput");
+    lwtahClear("SIADebug");
+    for(let i = 0; i < tags.length; i++){
+        lwtahAddRange("SIAInput", tags[i][0], tags[i][3]);
+        lwtahAddRange("SIAOutput", tags[i][1], tags[i][3]);
+        lwtahAddRange("SIADebug", tags[i][2], tags[i][3]);
     }   
-    console.log(errorStrings);
-    console.log(warningStrings);
 }
